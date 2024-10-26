13d2b5b1ba5303e2bdd46ff992dfbb153b33dba0
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2024-10-26 01:45:37 -0600

    Creduchain - First Commit
    A decentralized network for fast, secure credential verification. Coded by SOFIA TEAM.

---
+++ apps/static/assets/js/components/wallet/metamask.js
@@ -0,0 +1,20 @@
+document.getElementById('connect-wallet').addEventListener('click', async () => {
+// Comprobar si el navegador tiene una wallet instalada
+if (typeof window.ethereum !== 'undefined') {
+    try {
+    // Solicitar conexión a la wallet
+    web3 = new Web3(Web3.givenProvider || 'https://sepolia-rpc.scroll.io/');
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+
+    // Mostrar la dirección de la wallet conectada (puedes personalizar esto)
+    alert(`Wallet conectada: ${account}`);
+    } catch (error) {
+    console.error("Error al conectar a la wallet:", error);
+    alert('No se pudo conectar a la wallet. Asegúrate de que MetaMask esté instalado y habilitado.');
+    }
+} else {
+    alert('Por favor, instala MetaMask o otra wallet compatible.');
+}
+});
+
