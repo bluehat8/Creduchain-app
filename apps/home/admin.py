e9eaf89530f858e0f15a63f03f7813cc8e4cd127
RaveHunter05 <paulsotelor@yahoo.es>
2024-11-02 02:33:31 -0600

    feat: adding models to admin panel

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,5 +4,13 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
+from .models import Credential, Notification
 
 # Register your models here.
+@admin.register(Credential)
+class CredentialAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    pass
